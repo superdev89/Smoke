4f838e4e0a35b514c51f2bbbabfadc442cdb952a
hannesoud <hannesoud@outlook.com>
2017-11-16 18:27:41 +0800

    Iframe scrolling and remove comment

--- resources/views/pages/add_venue.blade.php
+++ resources/views/pages/add_venue.blade.php
@@ -237,7 +237,6 @@
 <script type="text/javascript" src="<?= asset('lib/webuploader/0.1.5/webuploader.min.js') ?>"></script>
 <script type="text/javascript" src="<?= asset('lib/ueditor/1.4.3/ueditor.config.js') ?>"></script>
 <script type="text/javascript" src="<?= asset('lib/ueditor/1.4.3/ueditor.all.min.js') ?>"></script>
-{{--<script type="text/javascript" src="<?= asset('lib/ueditor/1.4.3/lang/zh-cn/zh-cn.js') ?>"></script>--}}
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDSJff_oQeFbYObvFfMCUPdkzYf79LYkLo&libraries=places&callback=initAutocomplete"
         async defer></script>
